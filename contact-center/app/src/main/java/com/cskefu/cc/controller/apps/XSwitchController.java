825e802aa5fbb1b7c17acf29eda505b912738f4c
gavin <gav1n_wang@outlook.com>
2023-08-04 20:23:19 +0800

    fix

--- contact-center/app/src/main/java/com/cskefu/cc/controller/apps/XSwitchController.java
+++ contact-center/app/src/main/java/com/cskefu/cc/controller/apps/XSwitchController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.nats.client.Connection;
 import io.nats.client.Message;
+import io.nats.client.Nats;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -21,20 +22,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Controller
 @RequestMapping("/xswitch")
 public class XSwitchController extends Handler {
     private final static Logger logger = LoggerFactory.getLogger(XSwitchController.class);
-
-    @Autowired(required = false)
-    Connection natsConnection;
-
 
     @GetMapping("/conference/page")
     @Menu(type = "app", subtype = "profile", admin = true)
@@ -42,41 +44,40 @@ public class XSwitchController extends Handler {
         return request(super.createView("/apps/xswitch/conference_list"));
     }
 
-    @PostMapping("/conference/list")
-    @ResponseBody
-    @Menu(type = "app", subtype = "profile", admin = true)
-    public List<ConferenceList>conferenceInfo(ModelMap map, HttpServletRequest request, @Valid String snsid) {
-
-        List<ConferenceList> conferenceLists = new ArrayList<>();
-        conferenceLists.add(new ConferenceList("3000-xswitch.cn","3000",12));
-        conferenceLists.add(new ConferenceList("700037412-xswitch.cn","700037412",12));
-        conferenceLists.add(new ConferenceList("700037401-xswitch.cn","700037401",12));
-
-        return conferenceLists;
-    }
-
-
 //    @PostMapping("/conference/list")
 //    @ResponseBody
-//    @Async
 //    @Menu(type = "app", subtype = "profile", admin = true)
-//    public CompletableFuture<Message> conferenceInfo(ModelMap map, HttpServletRequest request, @Valid String snsid) throws JsonProcessingException {
-//        Map<String, String> data = new HashMap();
-//        data.put("jsonrpc","2.0");
-//        data.put("id","1");
-//        data.put("method","getConferenceList");
+//    public List<ConferenceList>conferenceInfo(ModelMap map, HttpServletRequest request, @Valid String snsid) {
 //
-//        String s = new ObjectMapper().writeValueAsString(data);
+//        List<ConferenceList> conferenceLists = new ArrayList<>();
+//        conferenceLists.add(new ConferenceList("3000-xswitch.cn","3000",12));
+//        conferenceLists.add(new ConferenceList("700037412-xswitch.cn","700037412",12));
+//        conferenceLists.add(new ConferenceList("700037401-xswitch.cn","700037401",12));
 //
-//        CompletableFuture<Message> messageCompletableFuture = natsConnection.request("cn.xswitch.cman.control", s.getBytes());
-//
-////        List<ConferenceList> conferenceLists = new ArrayList<>();
-////        conferenceLists.add(new ConferenceList("3000-xswitch.cn","3000",12));
-////        conferenceLists.add(new ConferenceList("700037412-xswitch.cn","700037412",12));
-////        conferenceLists.add(new ConferenceList("700037401-xswitch.cn","700037401",12));
-//
-//        return messageCompletableFuture;
+//        return conferenceLists;
 //    }
+
+
+    @PostMapping("/conference/list")
+    @ResponseBody
+    @Async
+    @Menu(type = "app", subtype = "profile", admin = true)
+    public CompletableFuture<Message> conferenceInfo(ModelMap map, HttpServletRequest request, @Valid String snsid) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+        Map<String, String> data = new HashMap();
+        data.put("jsonrpc", "2.0");
+        data.put("id", "1");
+        data.put("method", "getConferenceList");
+
+        String s = new ObjectMapper().writeValueAsString(data);
+
+        Connection nc = Nats.connect("nats://192.168.3.235:4222");
+        Message m = nc.request("greet.bob", null, Duration.ofSeconds(3));
+        System.out.println("Response received: " + new String(m.getData()));
+
+        CompletableFuture<Message> future = nc.request("cn.xswitch.cman.control", s.getBytes());
+        return future;
+
+    }
 
 
 }
